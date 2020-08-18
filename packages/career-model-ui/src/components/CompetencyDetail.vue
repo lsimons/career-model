8f7c983a5a31031227079d00c2d4d21c352bd251
Leo Simons <lsimons@schubergphilis.com>
2020-08-18 13:16:29 +0200

    WIP

--- packages/career-model-ui/src/components/CompetencyDetail.vue
+++ packages/career-model-ui/src/components/CompetencyDetail.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="competency-detail" style="width: 100%; height: 100%">
     <span v-html="detail"></span>
+    <div class="competency-definition">
+      <p>Coding is creating software by writing source code.</p>
+    </div>
+    <div class="competency-references">
+      <h6>References:</h6>
+      <ul>
+        <li><a data-isbn-13="978-0735619678" data-isbn-10="0735619670" href="https://www.amazon.com/Code-Complete-Practical-Handbook-Construction/dp/0735619670">Code Complete</a></li>
+        <li><a data-isbn-13="978-0132350884" data-isbn-10="9780132350884" href="https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship-dp-0132350882/dp/0132350882/">Clean Code</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -35,4 +45,7 @@ export default {
 </script>
 
 <style scoped>
+  .competency-definition {
+    font-weight: bold;
+  }
 </style>

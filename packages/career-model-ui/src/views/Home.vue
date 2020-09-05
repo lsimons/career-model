125aac6b671ac34fc88d86a588689e78a939dcaf
Leo Simons <mail@leosimons.com>
2020-09-05 12:34:11 +0200

    fix: responsive home page

--- packages/career-model-ui/src/views/Home.vue
+++ packages/career-model-ui/src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
   <v-container class="home d-flex flex-column" fluid style="min-height: 100%">
     <v-row class="flex-grow-1" dense>
-      <v-col cols="6">
+      <v-col cols="12" md="6">
         try 'Coding' to see a fleshed-out competency
-        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="3" category="Software Production" />
+        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="1" category="Software Production" />
       </v-col>
-      <v-col cols="6">
-        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="3" category="Software Management"/>
+      <v-col cols="12" md="6">
+        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="1" category="Software Management"/>
       </v-col>
     </v-row>
 
     <v-row class="flex-grow-1" dense>
-      <v-col cols="6">
-        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="3" category="Software Technology"/>
+      <v-col cols="12" md="6">
+        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="1" category="Software Technology"/>
       </v-col>
-      <v-col cols="6">
-        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="3" category="General"/>
+      <v-col cols="12" md="6">
+        <CompetencyGraph v-bind:width="200" v-bind:height="200" v-bind:max-level="1" category="General"/>
       </v-col>
     </v-row>
   </v-container>
